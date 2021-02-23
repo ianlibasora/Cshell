40371178fbe2ae9516935207fe01ad01951f49e9
libasoj2 <joseph.libasora2@mail.dcu.ie>
2021-02-23 12:33:09 +0000

    Added dir redirection detaction

--- src/dir.c
+++ src/dir.c
@@ -14,12 +14,32 @@
 // dir command, list contents of directory
 
 int dir(int lgt, char** lst, int out, bool detached) {
+   // Determine paths to directories
    char path[MAXPATH];
    if (lgt == 1 || (lgt == 2 && detached)) {
       strcpy(path, ".");
    } else {
       strcpy(path, lst[1]);
    }
+
+   // Determine redirection operation
+   char outFile[MAXPATH];
+   if (out != 0) {
+      // Check enough args
+      if (detached && lgt == 4) {
+         strcpy(outFile, lst[2]);
+      } else if (detached && lgt == 5) {
+         strcpy(outFile, lst[3]);
+      } else if (detached == false && lgt == 3) {
+         strcpy(outFile, lst[2]);
+      } else if (detached == false && lgt == 4) {
+         strcpy(outFile, lst[3]);
+      } else {
+         printf("Error. Incorrect arguments for redirection\n");
+         return 1;
+      }
+   }
+
 
    if (detached) {
       pid_t pid = fork();
@@ -30,7 +50,7 @@ int dir(int lgt, char** lst, int out, bool detached) {
          ls(path);
          exit(0);
       } else if (pid == -1) {
-         printf("Warning. Fork error occured\n");
+         printf("Error. Fork error occured\n");
          exit(1);
       }
       // Parent, does nothing, operation pushed to background
