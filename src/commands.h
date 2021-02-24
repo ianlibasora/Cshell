d1621bb20f5ea18ed51577267ae3da7d04aa7cbe
libasoj2 <joseph.libasora2@mail.dcu.ie>
2021-02-24 10:52:13 +0000

    Non plagerism statements

--- src/commands.h
+++ src/commands.h
@@ -1,4 +1,13 @@
 
+/*----------------------------------
+STATEMENT OF NON-PLAGIARISM
+
+I hereby declare that all information in this assignment has been obtained and presented in accordance with academic rules and ethical conduct and the work I am submitting in this document, except where I have indicated, is my own work.
+
+Student Number: 19307361
+Student Name: Joseph Libasora
+-----------------------------------*/
+
 #include <stdbool.h>
 
 // Headerfile for shell command functions
--- src/redirection.c
+++ src/redirection.c
@@ -1,4 +1,13 @@
 
+/*----------------------------------
+STATEMENT OF NON-PLAGIARISM
+
+I hereby declare that all information in this assignment has been obtained and presented in accordance with academic rules and ethical conduct and the work I am submitting in this document, except where I have indicated, is my own work.
+
+Student Number: 19307361
+Student Name: Joseph Libasora
+-----------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
