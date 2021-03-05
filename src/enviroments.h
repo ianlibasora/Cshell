3d8dedded208af6c96167313bfb2a4962140c1e8
libasoj2 <joseph.libasora2@mail.dcu.ie>
2021-03-05 16:03:43 +0000

    help.c refactor

--- src/enviroments.h
+++ src/enviroments.h
@@ -13,7 +13,3 @@
 void setShellENV(char* key, char* value);
 void setShellPath(char* key);
 void setExePath();
-void getHelpPath(char* fName);
-
-// System functions
-void getTermSize(short unsigned int* row, short unsigned int* col);
--- src/help.c
+++ src/help.c
@@ -14,164 +14,102 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #include "commands.h"
 #include "enviroments.h"
-#include "functions.h"
 
 #define MAXLINE 500
 #define MAXPATH 250
 
 // Help command
 
-// ----- NEED TO ADD MORE FILTER ------
+int help(char* outFile, int out, bool detached) {
+   pid_t pid = fork();
+   if (pid == 0) {
+      // Child
+      setShellENV("PARENT", getenv("SHELL"));
 
-void help(char* outFile, int out, bool detached) {
-   if (detached) {
-      pid_t pid = fork();
-      if (pid == 0) {
-         // Child
-         setShellENV("PARENT", getenv("SHELL"));
-
-         // Choose between either normal/redirection operation
-         out == 0 ? promptHelp(): helpRedirect(outFile, out);
-         exit(0);         
-      } else if (pid == -1) {
-         printf("Error. Fork error occured\n");
-         exit(1);
-      }
-      // Parent does nothing
-   } else {
       // Choose between either normal/redirection operation
-      out == 0 ? promptHelp(): helpRedirect(outFile, out);
-   }
-}
-
-int promptHelp() {
-   char line[MAXLINE];
-   int alloc = 100;
-   int lgt = 0;
-   char** lines = calloc(alloc, sizeof(char*));
-   if (lines == NULL) {
-      printf("Error. Memory allocation failed\n");
-      return 2;
-   }
-
-   // Read file contents into array
-   char* helpFile = calloc(MAXPATH, sizeof(char));
-   getHelpPath(helpFile);
-
-   FILE* fPtr = fopen(helpFile, "r");
-   if (fPtr != NULL) {
-      fgets(line, MAXLINE, fPtr);
-      while (!feof(fPtr)) {
-         lines[lgt] = calloc(strlen(line), sizeof(char));
-         line[strlen(line) - 1] = '\0';//Trim trailling \n
-         strcpy(lines[lgt], line);
-         fgets(line, MAXLINE, fPtr);
-         ++lgt;
-
-         if (lgt >= alloc) {
-            alloc += 100;
-            lines = realloc(lines, alloc * sizeof(char*));
-            if (lines == NULL) {
-               printf("Error. Memory reallocation failed\n");
-               return 3;
-            }
-         }
+      int ret = (out == 0 ? promptHelp(): helpRedirect(outFile, out));
+      if (ret) {
+         // If error raised
+         exit(2);
       }
+      exit(0);         
+   } else if (pid == -1) {
+      fprintf(stderr, "Error. Fork error occured\n");
+      exit(1);
    } else {
-      printf("Error. Error accessing %s\n", helpFile);
-      return 1;
-   }
-   fclose(fPtr);
-
-   // More filter
-   short unsigned int row, col;
-   getTermSize(&row, &col);
-   int bufferEnd = 0;// Count of number of buffer rows used by printing
-   int i = 0;// Index of lines array
-   bool overflow = false;
-   int offset = 0;
-   char* tmp;
-
-   while (i < lgt) {
-      if (overflow || bufferEnd >= row) {
-         // Reached max amount of available rows on the screen
-         bufferEnd = 0;
-         overflow = false;
-
-         // Prompt interferes with output
-         tmp = getpass(" ");
-      }
-
-      if (strlen(lines[i]) > col) {
-         offset = calcOffset(strlen(lines[i]), col);
-         if (bufferEnd + offset >= row) {
-            // Account for cases of lines overflow
-            overflow = true;
-         } else {
-            printf("%s", lines[i]);
-            // If not final print before buffering, print \n 
-            // This is to not cause a \n with getchar()
-            if (bufferEnd + offset < row) {
-               printf("\n");
-            }
-            
-            bufferEnd += offset;
-            ++i;
-         }
-      } else if (bufferEnd + 1 == row) {
-         // Case: Final print before buffering will trigger
-         // Dont print new line
-         // This is to not cause a \n with getchar()
-         printf("%s", lines[i]);
-         ++i;
-         ++bufferEnd;
-      } else {
-         printf("%s\n", lines[i]);
-         ++i;
-         ++bufferEnd;
+      // Parent
+      if (!detached) {
+         // If not running detached
+         wait(NULL);
       }
    }
-
-   // Clean memory used
-   clearArgs(lgt, lines);
-   free(lines);
-   free(helpFile);
-   free(tmp);
    return 0;
 }
 
-void helpRedirect(char* outFile, int out) {
-   char op[4];
-   strcpy(op, (out == 1 ? "w": "a"));
-   char line[MAXLINE];
+int promptHelp() {
+   char* helpFile = calloc(MAXPATH, sizeof(char));
+   getHelpPath(helpFile);
+   char* cmd[] = {"more", helpFile, NULL};
+   execvp(cmd[0], cmd);
 
-   FILE* outFilePtr = fopen(outFile, op);
+   free(helpFile);
+   return 0;
+}
+
+int helpRedirect(char* outFile, int out) {
+   char line[MAXLINE];
+   char* helpFile = calloc(MAXPATH, sizeof(char));
+   getHelpPath(helpFile);
+   
+   // Ternary operator: chooses between `w` or `a`
+   FILE* outFilePtr = fopen(outFile, (out == 1 ? "w": "a"));
+
    if (outFilePtr != NULL) {
-      FILE* manFilePtr = fopen("../manual/readme.md", "r");
+      FILE* manFilePtr = fopen(helpFile, "r");
+      
       if (manFilePtr != NULL) {
          fgets(line, MAXLINE, manFilePtr);
          while (!feof(manFilePtr)) {
             fprintf(outFilePtr, "%s", line);
             fgets(line, MAXLINE, manFilePtr);
          }
+
       } else {
-         printf("Error. Error accessing ../manual/readme.md\n");
+         fprintf(stderr, "Error. Error accessing helpfile\n");
+         return 1;
       }
       fclose(manFilePtr);
+
    } else {
-      printf("Error. Error accessing %s\n", outFile);
+      fprintf(stderr, "Error. Error accessing %s\n", outFile);
+      return 1;
    }
+   free(helpFile);
    fclose(outFilePtr);
+   return 0;
 }
 
-int calcOffset(int lgt, int maxBuffer) {
-   int out = 1;
-   while (lgt > maxBuffer) {
-      lgt -= maxBuffer;
-      ++out;
+void getHelpPath(char* fName) {
+   char relativePath[] = "/manual/readme.md";
+   char* path = strdup(getenv("SHELL"));
+   path = realloc(path, (strlen(path) + 5) * sizeof(char));
+   
+   // Transform path to exe, to path to help readme.md file
+   int i = strlen(path) - 1;
+   int count = 0;
+   while (0 <= i && count < 2) {
+      // Go back 1 directory
+      if (path[i] == '/') {
+         ++count;
+      }
+      path[i] = '\0';
+      --i;
    }
-   return out;
+   strcat(path, relativePath);// Append relative path to full path
+   strcpy(fName, path);
+   // Note: memory allocated for `path` is freed by the caller
 }
