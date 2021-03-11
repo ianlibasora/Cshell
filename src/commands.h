fb8bfc52fee7152163cd68e31b065e830c7bb0f5
libasoj2 <joseph.libasora2@mail.dcu.ie>
2021-03-11 10:46:38 +0000

    Detachment rework

--- src/commands.h
+++ src/commands.h
@@ -17,26 +17,26 @@ int cd(int lgt, char** inp, bool detached);
 void changeDir(char* path);
 
 // echo
-int echo(int lgt, char** inp, char* outFile, int out, bool detached, int* killPID);
+int echo(int lgt, char** inp, char* outFile, int out, int* killPID);
 int echoRedirect(int lgt, char** lst, char* outFile, int out);
 
 // environ
-int listENV(char* outFile, int out, bool detached);
+int listENV(char* outFile, int out);
 int listENVRedirect(char** envs, char* outFile, int out);
 
 // dir
-int dir(int lgt, char** lst, char* outFile, int out, bool detached, int* killPID);
+int dir(int lgt, char** lst, char* outFile, int out, int* killPID);
 int ls(char* path);
 int lsRedirected(char* path, char* outFile, int out);
 
 // fallback execution
-int fallbackChild(int lgt, char** lst, char* inFile, bool in, char* outFile, int out, bool detached, int* killPID);
+int fallbackChild(int lgt, char** lst, char* inFile, bool in, char* outFile, int out, int* killPID);
 
 // pause
-int pauseShell(bool detached, int* killPID);
+int pauseShell(int* killPID);
 
 // Help
-int help(char* outFile, int out, bool detached, int* killPID);
+int help(char* outFile, int out, int* killPID);
 int promptHelp();
 int helpRedirect(char* outFile, int out);
 void getHelpPath(char* fName);
