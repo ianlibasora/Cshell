d78dd68f9b3646b9b4573d5d92375c8cee799e30
Joseph Libasora <56083316+ianlibasora@users.noreply.github.com>
2021-05-19 10:13:53 +0100

    Added header guards

--- src/enviroments.h
+++ src/enviroments.h
@@ -1,6 +1,11 @@
 
 // Headerfile for enviroment variable manipulation functions
 
+#ifndef ENVIRONMENTS_H
+#define ENVIRONMENTS_H
+
 void setShellENV(char* key, char* value);
 void setShellPath(char* key);
 void setExePath();
+
+#endif
--- src/functions.h
+++ src/functions.h
@@ -3,6 +3,9 @@
 
 // Headerfile for functions handling shell operation (main.c)
 
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
 char* promptInput(bool prompt);
 int splitString(char* str, int* lgt, char** lst, int max);
 void clearArgs(int lgt, char** lst);
@@ -11,3 +14,5 @@ bool checkInvalidString(char* str);
 
 // Matchmode function
 int batchRunner(char* fName);
+
+#endif
